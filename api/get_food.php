45509dca8efad35ca765af7b0b5c871d19b2f80d
Pongsathon Somjai <pongsathon_pk_202@hotmail.com>
2018-01-25 18:05:32 +0700

    add food search by name

---
+++ api/get_food.php
@@ -0,0 +1,14 @@
+<?php
+        include '../config/connection.php';
+        
+        $food_name = $_POST["food"];
+        $sql = "SELECT food_name, food_type FROM `tbl_food` WHERE food_name LIKE '%$food_name%'";
+        $query = mysqli_query($conn, $sql);
+        $json = array();
+        while ($row = mysqli_fetch_array($query)){
+            $arr = array("food_name" => $row["food_name"],
+                         "food_type" => $row["food_type"]);
+            array_push($json, $arr);
+        }
+        echo json_encode($json);
+?>
